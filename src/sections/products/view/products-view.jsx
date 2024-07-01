cc7bf1f8a27b0e66c899abd1b045959f357d71e0
krishpatel1610 <115217085+krishpatel1610@users.noreply.github.com>
2024-07-01 10:44:59 +0530

    final

--- src/sections/products/view/products-view.jsx
+++ src/sections/products/view/products-view.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 import Box from '@mui/material/Box';
 import Step from '@mui/material/Step';
+import Chip from '@mui/material/Chip';
 import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
 import Stepper from '@mui/material/Stepper';
+import MenuItem from '@mui/material/MenuItem';
 import StepLabel from '@mui/material/StepLabel';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 const steps = [
   'Recipe Details',
@@ -18,7 +23,7 @@ const steps = [
 ];
 
 export default function ProductsView() {
-  const [activeStep, setActiveStep] = useState(0); // Start activeStep from 0
+  const [activeStep, setActiveStep] = useState(0);
   const [formData, setFormData] = useState({
     name: '',
     description: '',
@@ -26,7 +31,42 @@ export default function ProductsView() {
     prep_time: '',
     serving_persons: '',
     difficulty: '',
+    cuisine_type: [],
+    dietary: [],
+    meal_type: [],
   });
+  const [customCuisine, setCustomCuisine] = useState('');
+  const [customDietary, setCustomDietary] = useState('');
+  const [customMealType, setCustomMealType] = useState('');
+  const [cuisineOptions, setCuisineOptions] = useState([
+    'Italian',
+    'Indian',
+    'Chinese',
+    'Japanese',
+    'Mexican',
+    'French',
+    'Mediterranean',
+    'Custom',
+  ]);
+  const [dietaryOptions, setDietaryOptions] = useState([
+    'Vegan',
+    'Vegetarian',
+    'Pescatarian',
+    'Gluten-free',
+    'Lactose-free',
+    'Sugar-free',
+    'Non-vegetarian',
+    'Custom',
+  ]);
+  const [mealTypeOptions, setMealTypeOptions] = useState([
+    'Breakfast',
+    'Brunch',
+    'Lunch',
+    'Dinner',
+    'Snack',
+    'Dessert',
+    'Custom',
+  ]);
 
   useEffect(() => {
     console.log(activeStep);
@@ -48,54 +88,99 @@ export default function ProductsView() {
     }));
   };
 
+  const handleCustomCuisineChange = (e) => {
+    setCustomCuisine(e.target.value);
+  };
+
+  const handleCustomDietaryChange = (e) => {
+    setCustomDietary(e.target.value);
+  };
+
+  const handleCustomMealTypeChange = (e) => {
+    setCustomMealType(e.target.value);
+  };
+
+  const handleAddCustomCuisine = () => {
+    if (customCuisine && !cuisineOptions.includes(customCuisine)) {
+      setCuisineOptions((prevOptions) => [...prevOptions, customCuisine]);
+      setFormData((prevData) => ({
+        ...prevData,
+        cuisine_type: [...prevData.cuisine_type, customCuisine],
+      }));
+      setCustomCuisine('');
+    }
+  };
+
+  const handleAddCustomDietary = () => {
+    if (customDietary && !dietaryOptions.includes(customDietary)) {
+      setDietaryOptions((prevOptions) => [...prevOptions, customDietary]);
+      setFormData((prevData) => ({
+        ...prevData,
+        dietary: [...prevData.dietary, customDietary],
+      }));
+      setCustomDietary('');
+    }
+  };
+
+  const handleAddCustomMealType = () => {
+    if (customMealType && !mealTypeOptions.includes(customMealType)) {
+      setMealTypeOptions((prevOptions) => [...prevOptions, customMealType]);
+      setFormData((prevData) => ({
+        ...prevData,
+        meal_type: [...prevData.meal_type, customMealType],
+      }));
+      setCustomMealType('');
+    }
+  };
+
   const handleReset = () => {
-    setActiveStep(0); // Reset activeStep to 0
+    setActiveStep(0);
     setFormData({
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: '',
-      addressLine1: '',
-      addressLine2: '',
-      city: '',
-      state: '',
-      zipCode: '',
       name: '',
       description: '',
       cook_time: '',
       prep_time: '',
       serving_persons: '',
       difficulty: '',
+      cuisine_type: [],
+      dietary: [],
+      meal_type: [],
     });
+    setCuisineOptions([
+      'Italian',
+      'Indian',
+      'Chinese',
+      'Japanese',
+      'Mexican',
+      'French',
+      'Mediterranean',
+      'Custom',
+    ]);
+    setDietaryOptions([
+      'Vegan',
+      'Vegetarian',
+      'Pescatarian',
+      'Gluten-free',
+      'Lactose-free',
+      'Sugar-free',
+      'Non-vegetarian',
+      'Custom',
+    ]);
+    setMealTypeOptions([
+      'Breakfast',
+      'Brunch',
+      'Lunch',
+      'Dinner',
+      'Snack',
+      'Dessert',
+      'Custom',
+    ]);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here
     console.log('Form Submitted:', formData);
-    // Optionally, add redirection logic or API calls for form submission
-
-    // Reset form data
-    setFormData({
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: '',
-      addressLine1: '',
-      addressLine2: '',
-      city: '',
-      state: '',
-      zipCode: '',
-      name: '',
-      description: '',
-      cook_time: '',
-      prep_time: '',
-      serving_persons: '',
-      difficulty: '',
-    });
-
-    // Reset active step to 0
-    setActiveStep(0);
+    handleReset();
   };
 
   const getStepContent = (step) => {
@@ -188,22 +273,117 @@ export default function ProductsView() {
         return (
           <>
             <Typography variant="h6">Step 2: Category Information</Typography>
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel>Cuisine Type</InputLabel>
+              <Select
+                name="cuisine_type"
+                multiple
+                value={formData.cuisine_type}
+                onChange={handleChange}
+                renderValue={(selected) => (
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+              >
+                {cuisineOptions.map((cuisine) => (
+                  <MenuItem key={cuisine} value={cuisine}>
+                    {cuisine}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
             <TextField
-              name="email"
-              label="Email"
-              value={formData.email}
-              onChange={handleChange}
+              name="custom_cuisine"
+              label="Add Custom Cuisine"
+              value={customCuisine}
+              onChange={handleCustomCuisineChange}
               fullWidth
               margin="normal"
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  handleAddCustomCuisine();
+                }
+              }}
             />
+
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel>Dietary Preferences</InputLabel>
+              <Select
+                name="dietary"
+                multiple
+                value={formData.dietary}
+                onChange={handleChange}
+                renderValue={(selected) => (
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+              >
+                {dietaryOptions.map((dietary) => (
+                  <MenuItem key={dietary} value={dietary}>
+                    {dietary}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
             <TextField
-              name="phoneNumber"
-              label="Phone Number"
-              value={formData.phoneNumber}
-              onChange={handleChange}
+              name="custom_dietary"
+              label="Add Custom Dietary"
+              value={customDietary}
+              onChange={handleCustomDietaryChange}
               fullWidth
               margin="normal"
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  handleAddCustomDietary();
+                }
+              }}
             />
+
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel>Meal Type</InputLabel>
+              <Select
+                name="meal_type"
+                multiple
+                value={formData.meal_type}
+                onChange={handleChange}
+                renderValue={(selected) => (
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                    {selected.map((value) => (
+                      <Chip key={value} label={value} />
+                    ))}
+                  </Box>
+                )}
+              >
+                {mealTypeOptions.map((mealType) => (
+                  <MenuItem key={mealType} value={mealType}>
+                    {mealType}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <TextField
+              name="custom_meal_type"
+              label="Add Custom Meal Type"
+              value={customMealType}
+              onChange={handleCustomMealTypeChange}
+              fullWidth
+              margin="normal"
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  handleAddCustomMealType();
+                }
+              }}
+            />
+
             <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
               <Button disabled={activeStep === 0} onClick={handleBack} variant="outlined" color="inherit">
                 Back
@@ -214,94 +394,42 @@ export default function ProductsView() {
             </Box>
           </>
         );
+
       case 2:
         return (
           <>
             <Typography variant="h6">Step 3: Add Nutrition Level</Typography>
-            <TextField
-              name="addressLine1"
-              label="Address Line 1"
-              value={formData.addressLine1}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              name="addressLine2"
-              label="Address Line 2"
-              value={formData.addressLine2}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              name="city"
-              label="City"
-              value={formData.city}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              name="state"
-              label="State"
-              value={formData.state}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              name="zipCode"
-              label="Zip Code"
-              value={formData.zipCode}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-              <Button disabled={activeStep === 0} onClick={handleBack} variant="outlined" color="inherit">
-                Back
-              </Button>
-              <Button onClick={handleNext} variant="contained" color="primary" type="button">
-                Next
-              </Button>
-            </Box>
+            {/* ...fields for Step 3 */}
           </>
         );
+
       case 3:
         return (
           <>
             <Typography variant="h6">Step 4: Add Instructions</Typography>
-            {/* Add fields for instructions */}
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
-              <Button disabled={activeStep === 0} onClick={handleBack} variant="outlined" color="inherit">
-                Back
-              </Button>
-              <Button onClick={handleNext} variant="contained" color="primary" type="button">
-                Next
-              </Button>
-            </Box>
+            {/* ...fields for Step 4 */}
           </>
         );
+
       case 4:
         return (
           <>
             <Typography variant="h6">Step 5: Add Ingredients</Typography>
-            {/* Add fields for ingredients */}
+            {/* ...fields for Step 5 */}
             <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 2 }}>
               <Button disabled={activeStep === 0} onClick={handleBack} variant="outlined" color="inherit">
                 Back
               </Button>
               <Button onClick={handleReset} variant="outlined" color="secondary">
-                   Reset
-                 </Button>
+                Reset
+              </Button>
               <Button onClick={handleSubmit} variant="contained" color="primary" type="button">
                 Finish
               </Button>
             </Box>
           </>
         );
-        
+
       default:
         return 'Unknown step';
     }
@@ -321,10 +449,8 @@ export default function ProductsView() {
         ))}
       </Stepper>
 
-      <Box sx={{ mb: 2 }}>
-        <form onSubmit={handleSubmit}>
-          <Box>{getStepContent(activeStep)}</Box>
-        </form>
+      <Box component="form" onSubmit={handleSubmit}>
+        {getStepContent(activeStep)}
       </Box>
     </Container>
   );
